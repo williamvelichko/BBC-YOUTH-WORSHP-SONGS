dc0c6ed00da63ebacd6b4482a907f370c8fc9cf8
williamvelichko <williamvelichko2003@gmail.com>
2023-08-16 16:55:03 -0700

    save changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 interface HeaderProps {
@@ -13,34 +13,32 @@ const Header: React.FC<HeaderProps> = ({ onSearch }) => {
 
   return (
     <header className="bg-gray-900 py-4 text-white">
-      <div className="container mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-between">
-        <div className="flex items-center justify-between w-full md:w-auto mb-4 md:mb-0">
-          <h1 className="text-2xl font-bold text-yellow-500">
+      <div className="container mx-auto px-4 md:px-6">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          <h1 className="text-2xl font-bold text-yellow-500 mb-4 md:mb-0">
             BBC Worship Songs
           </h1>
+          <div className="md:flex md:items-center space-x-4 w-full md:w-auto">
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-white transition duration-300"
+            >
+              All Songs
+            </Link>
+            <Link
+              to="/add-song"
+              className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300"
+            >
+              Add Song
+            </Link>
+          </div>
+          <input
+            className="w-full border rounded px-4 py-2 focus:outline-none placeholder-gray-400 text-black mt-4 md:mt-0"
+            type="text"
+            placeholder="Search songs..."
+            onChange={handleInputChange}
+          />
         </div>
-        <div className="flex items-center space-y-4 md:space-y-0 md:space-x-4 w-full md:w-auto">
-          <Link
-            to="/"
-            className="text-gray-300 hover:text-white transition duration-300"
-          >
-            All Songs
-          </Link>
-          <Link
-            to="/add-song"
-            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300"
-          >
-            Add Song
-          </Link>
-        </div>
-      </div>
-      <div className="container mx-auto mt-4">
-        <input
-          className="w-full border rounded px-4 py-2 focus:outline-none placeholder-gray-400 text-black"
-          type="text"
-          placeholder="Search songs..."
-          onChange={handleInputChange}
-        />
       </div>
     </header>
   );
