30edd4a554fee51f60f728aefeb0bba50520a3ac
williamvelichko <williamvelichko2003@gmail.com>
2023-08-25 17:36:58 -0700

    side bar

--- src/components/Firebase/firebase-Auth.tsx
+++ src/components/Firebase/firebase-Auth.tsx
@@ -1,37 +1,52 @@
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+import { useState, useEffect } from "react";
+import { signInWithPopup, User } from "firebase/auth";
 import { auth, provider } from "./firebase-config";
 
-export const signInWithGoogle = async () => {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    console.log("Google Sign-In Successful:", result.user);
-  } catch (error) {
-    console.error("Google Sign-In Error:", error);
-  }
+const useFirebaseAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("Google Sign-In Successful:", result.user);
+    } catch (error) {
+      console.error("Google Sign-In Error:", error);
+    }
+  };
+
+  const signOutFromGoogle = async () => {
+    try {
+      await auth.signOut();
+      console.log("Logout Successful");
+    } catch (error) {
+      console.error("Sign-Out Error:", error);
+    }
+  };
+
+  const checkLoggedIn = () => {
+    const currentUser = auth.currentUser;
+    return !!currentUser;
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setIsLoggedIn(!!user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return {
+    user,
+    isLoggedIn,
+    signInWithGoogle,
+    signOutFromGoogle,
+    checkLoggedIn,
+  };
 };
 
-export const signOutFromGoogle = async () => {
-  try {
-    const user = await auth.signOut();
-    console.log(user, "logout Successful");
-  } catch (error) {
-    console.error("Sign-Out Error:", error);
-  }
-};
-
-export const checkLoggedIn = () => {
-  const user = auth.currentUser;
-
-  if (user) {
-    console.log("User is logged in:", user);
-    return true;
-  } else {
-    console.log("User is not logged in");
-    return false;
-  }
-};
+export default useFirebaseAuth;
--- src/components/Firebase/firebase-config.ts
+++ src/components/Firebase/firebase-config.ts
@@ -17,13 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
-
-export const signInWithGoogle = () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
