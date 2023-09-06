0ea074af595eefc8b373f35e492ac4b760740e8a
williamvelichko <williamvelichko2003@gmail.com>
2023-09-06 11:42:43 -0700

    new save

--- src/components/Logic/ChordTransposer.tsx
+++ src/components/Logic/ChordTransposer.tsx
@@ -1,4 +1,3 @@
-// ChordTransposer.tsx
 import React from "react";
 
 export interface ChordTransposerProps {
@@ -36,25 +35,57 @@ const ChordTransposer: React.FC<ChordTransposerProps> = ({
     return transposeChords[index];
   };
 
-  const transposedText = children.replace(
-    /\[([A-G][#b]?)([^[\]]*)\]/g,
-    (_, chord, rest) => {
-      const transposedChord = transposeChord(chord);
-      return `[${transposedChord}${rest}]`;
-    }
-  );
-  console.log(transposedText.split("\n"));
-  const lines = transposedText.split("\n").map((line, index) => (
-    <p key={index} className="chord-line">
-      {line.split("\n\n").map((verse, vIndex) => (
-        <span key={vIndex} className="chord-verse">
-          {verse}
-        </span>
-      ))}
-    </p>
-  ));
+  // Split the children string by '\\n\n' for verses/paragraphs and '\\n' for lines
+  const verses = children.split("\\n\\n");
 
-  return <div className="whitespace-pre-line">{lines}</div>;
+  const lines = verses.map((verse, vIndex) => {
+    const linesInVerse = verse.split("\\n");
+
+    const verseContent = linesInVerse.map((line, lIndex) => {
+      const chords = line.match(/\[.*?\]/g);
+      console.log(chords);
+      if (chords) {
+        const chordLine = chords
+          .map((chord) => transposeChord(chord.replace(/\[|\]/g, "")))
+          .join("\t");
+
+        const lyricLine = line.replace(/\[.*?\]/g, "");
+
+        return (
+          <div key={lIndex} className="chord-line">
+            <div className="chords">
+              <span className="chord">{chordLine}</span>
+            </div>
+            <div className="lyrics">
+              <span className="lyric">{lyricLine}</span>
+            </div>
+          </div>
+        );
+      }
+
+      // If the line is empty or only contains spaces, render it as an empty line
+      if (line.trim() === "") {
+        return <div key={lIndex} className="empty-line" />;
+      }
+
+      // If the line is non-empty and doesn't contain chords, render it as pure lyrics
+      return (
+        <div key={lIndex} className="lyric-line">
+          <div className="lyrics">
+            <span className="lyric">{line}</span>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div key={vIndex} className="verse">
+        {verseContent}
+      </div>
+    );
+  });
+
+  return <div className="whitespace-pre-line space-y-2">{lines}</div>;
 };
 
 export default ChordTransposer;
